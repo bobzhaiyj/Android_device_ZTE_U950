6dda87481673b8aca5274bfdbdcb46b28f890eba
bobzhaiyj <bobzhaiyj@163.com>
2014-10-07 18:37:55 +0800

    Update CameraWrapper.cpp

--- camerawrapper/CameraWrapper.cpp
+++ camerawrapper/CameraWrapper.cpp
@@ -55,7 +55,7 @@ camera_module_t HAL_MODULE_INFO_SYM = {
          version_major: 1,
          version_minor: 0,
          id: CAMERA_HARDWARE_MODULE_ID,
-         name: "ZTE U5 Camera Wrapper",
+         name: "ZTE V985 Camera Wrapper",
          author: "Focus.Lau <focus.lau@gmail.com>",
          methods: &camera_module_methods,
          dso: NULL, /* remove compilation warnings */
